4288fa60817978eb6f02bb896684d29c47ced7e3
febriaricandra <febriaricandraproduction@gmail.com>
2024-11-24 13:53:30 +0700

    add workflows

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import BookService from '../services/BookService'
 
 export default function Home() {
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Register() {
     return (
         <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
