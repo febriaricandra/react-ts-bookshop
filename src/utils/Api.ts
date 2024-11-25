2f236fe2a33a2d604018b2503980924e040f557c
febriaricandra <febriaricandraproduction@gmail.com>
2024-11-25 16:53:27 +0700

    Cors policy

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -5,6 +5,7 @@ const Api = axios.create({
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
+        "Access-Control-Allow-Origin": "*",
     },
 });
 
