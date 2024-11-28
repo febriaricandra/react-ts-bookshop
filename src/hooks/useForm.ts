cd144d8cdd05a426ae3d169c88fe0fc43c81e9c1
febriaricandra <febriaricandraproduction@gmail.com>
2024-11-28 15:25:01 +0700

    custom hooks: useform

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface ValidationErrors {
+  [key: string]: string;
+}
+
+interface UseFormProps<T> {
+  initialValues: T;
+  validate: (values: T) => ValidationErrors;
+  onSubmit: (values: T) => void;
+}
+
+export const useForm = <T extends object>({
+  initialValues,
+  validate,
+  onSubmit,
+}: UseFormProps<T>) => {
+  const [values, setValues] = useState<T>(initialValues);
+  const [errors, setErrors] = useState<ValidationErrors>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const validationErrors = validate(values);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({});
+      onSubmit(values);
+    }
+  };
+
+  return { values, errors, handleChange, handleSubmit };
+};
